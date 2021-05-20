930704cb6f7036d300c99cb8382ae6f1fe7d44a3
kirti18patel <kirti18patel@yahoo.com>
2021-05-20 16:41:50 -0400

    comments added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,38 +1,43 @@
 // getting current date
 var currentDate = moment().format('dddd, MMMM Do');
-var currentHour = moment().format('h A');
 // updated current date in page
 $("#currentDay").text(currentDate);
 
 var statusEl;
-
 var timeAllDay = ["9 AM", "10 AM", "11 AM", "12 PM", "1 PM", "2 PM", "3 PM", "4 PM", "5 PM"];
 
+// save data in localstorage
 var saveTasks = function() {
     localStorage.setItem("tasks", JSON.stringify(tasks));
 };
 
+// load data from localstorage
 var loadTasks = function() {
     tasks = JSON.parse(localStorage.getItem("tasks")) || [];
     tasks.forEach(element => $("#"+element.id)
     .text(element.task));
 };
 
+// display all available business ime slot when page loaded
 var displayAll = function(){
     for( var i=0; i<timeAllDay.length; i++)
     {   
+        // create each row
         var rowEl = $("<div>")
         .addClass("row");
 
+        // create hourEl
         var hourEl = $("<div>")
     .addClass("col-xl-2 hour p-3 text-right")
     .text(timeAllDay[i]);
 
+    // create statusEl
     statusEl = $("<div>")
     .addClass("col-xl-8 p-3")
     .text("")
     .attr("id",i);
 
+    // set background color of statusEl depends on time
     if((i+9) == moment().hours()){
         statusEl.addClass("present");
     }
@@ -43,6 +48,7 @@ var displayAll = function(){
         statusEl.addClass("past");
     }
 
+    // create save button for each row
     var saveBtnEl = $("<div>")
     .addClass("col-xl-2 saveBtn p-3 text-center");
     saveBtnEl.append('<i class="fa fa-floppy-o"></i>');
@@ -55,6 +61,7 @@ var displayAll = function(){
 displayAll();
 loadTasks();
 
+// convert div element to input when user clicked on it
 var handleTaskClick = function(){
     // get current text in task status box
     var taskText = $(this)
@@ -67,12 +74,16 @@ var handleTaskClick = function(){
     .text(taskText)
     .val(taskText);
 
+    // call itself when clicked on it
     taskTextInput.on("click", handleTaskClick);
     $(this).replaceWith(taskTextInput);
     taskTextInput.trigger("focus");
 };
+
+// click event listener
 $(".col-xl-8").on("click", handleTaskClick);
 
+// update data entered by user
 var displayChangedData = function(){
     $(".row").on("change", "input[type='text']", function() {
     // get  text and position in the list
@@ -90,9 +101,11 @@ var displayChangedData = function(){
     .addClass($(this).attr("class"))
     .text(taskTextFromInput);
 
+    // event listener for statusEl when get clicked
     statusEl.on("click", handleTaskClick);
     $(this).replaceWith(statusEl);
 });
 }
 
+// event listener to save data when save button get clicked
 $(".saveBtn").on("click", displayChangedData);
